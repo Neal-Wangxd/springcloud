7aac4b04849342395548050f1f6c3f9ac8377ccd
Neal-Wangxd <wangxd_work@163.com>
2018-01-30 16:54:22 +0800

    dept实体类

---
+++ microcloud-api/src/main/java/com/microcloud/api/bean/Dept.java
@@ -0,0 +1,52 @@
+package com.microcloud.api.bean;
+
+import java.io.Serializable;
+
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年1月30日 下午2:33:34 
+* 类说明 : dept实体类
+*/
+@SuppressWarnings("serial")
+public class Dept implements Serializable {
+	
+	private Long deptno;
+	private String dname;
+	private String loc;
+	
+	public Long getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(Long deptno) {
+		this.deptno = deptno;
+	}
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	public Dept(Long deptno, String dname, String loc) {
+		super();
+		this.deptno = deptno;
+		this.dname = dname;
+		this.loc = loc;
+	}
+	public Dept() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Dept [deptno=" + deptno + ", dname=" + dname + ", loc=" + loc + "]";
+	}
+	
+}
+ 

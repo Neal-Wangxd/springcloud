854cd7339aa20f90ba097c556e5526cfd66233c5
Neal-Wangxd <wangxd_work@163.com>
2018-01-31 09:56:07 +0800

    修改了启动类的名称

---
+++ microcloud-provider-dept-8001/src/main/java/com/microcloud/provider/StartSpringCloudProviderMain.java
@@ -0,0 +1,19 @@
+package com.microcloud.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年1月30日 下午3:35:10 
+* 类说明 
+*/
+@SpringBootApplication
+public class StartSpringCloudProviderMain {
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpringCloudProviderMain.class, args);
+    }
+}
+ 

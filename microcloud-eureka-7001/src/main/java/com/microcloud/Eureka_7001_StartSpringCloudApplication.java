ded86f31b52cf3216e8509526588992cfa458acd
Neal-Wangxd <wangxd_work@163.com>
2018-02-26 11:33:37 +0800

    add eureak 7001 project

---
+++ microcloud-eureka-7001/src/main/java/com/microcloud/Eureka_7001_StartSpringCloudApplication.java
@@ -0,0 +1,21 @@
+package com.microcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年2月26日 下午3:35:10 
+* 类说明 
+*/
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka_7001_StartSpringCloudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka_7001_StartSpringCloudApplication.class, args);
+    }
+}
+ 
